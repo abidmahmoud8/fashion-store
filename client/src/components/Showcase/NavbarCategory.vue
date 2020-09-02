7e3946eb0c3d68cbbfd1712b28d461a0669979c5
abidmahmoud8 <abidmahmoud8@gmail.com>
2020-09-03 00:30:30 +0100

    fix navbar category

--- client/src/components/Showcase/NavbarCategory.vue
+++ client/src/components/Showcase/NavbarCategory.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="NavbarCategory" @click="ok">
+  <div id="NavbarCategory">
     <div class="text-center py-2">
-      <!-- <v-menu open-on-hover offset-y v-for="menu in this.menus" :key="menu.id" >
+      <v-menu open-on-hover offset-y v-for="menu in this.menus" :key="menu.id" >
         <template v-slot:activator="{ on, attrs }">
-          <v-btn v-bind="attrs" v-on="on" @click="goto(menu.id)" v-if="menu.level == 1">
+          <v-btn v-bind="attrs" v-on="on" @click="goto(menu.id)">
             {{menu.name}}
           </v-btn>
         </template>
@@ -15,93 +15,41 @@
               </v-list-item>
             </v-list>
           </v-col>
+
         </v-row>
-      </v-menu> -->
+      </v-menu>
     </div>
 
   </div>
 
 </template>
 <script>
-
-  import gql from 'graphql-tag';
-const myQuery = gql`
-  query categories {
-    categories{
-      id
-   }
-}`
-
+import axios from 'axios';
   export default {
-    // apollo: {
-    //   category: {
-    //     query: gql`categories{id}`,
-        // Static parameters
-        // variables () {
-        //   // Use vue reactive properties here
-        //   return {
-        //       id: 1,
-        //   }
-        // },
-    //   }
-    // },
-    data() {
-      return {
-        categoriesfilter : [],
-        menus : [],
-      }
-    },  
-  apollo: {
-    categories: gql`
-      query {
-        categories {
-          name
-          id
-        }
-      }
-    `,
-  },
-
-
-    mounted() {
-      this.$apollo.queries.books.refetch();
-        this.categoriesfilter = this.categories.filter(category => category.gendre.charAt(0) == this.$route.path.substring(1).charAt(0))
-        this.menus = []
-        this.menus = this.categoriesfilter[0].children
-        this.categoriesfilter.forEach(element => {
-            this.menus.push(element)
-        });
-
-
+    name: "NavbarCategory",
+    
+    data: () => ({
+      menus: ['categories-1', 'categories-2', 'categories-3', 'categories-4', 'categories-5']
+    }),
+     beforeMount() {
+      axios.get('http://localhost:4000/api/category/recursive')
+        .then(response => {
+          this.categories = response.data
+          this.categoriesfilter = this.categories.filter(category => category.gendre.charAt(0) == this.$route.path.substring(1).charAt(0));
+          this.menus = this.categoriesfilter[0].children;
+       })
     },
     watch: {
       $route() {
-          this.categoriesfilter = this.categories.filter(category => category.gendre.charAt(0) == this.$route.path.substring(1).charAt(0))
-          this.menus = []
-          this.menus = this.categoriesbyGendre[0].children
-          this.categoriesfilter.forEach(element => {
-              this.menus.push(element)
-          });
-          
-      },
-      categories() {
         this.categoriesfilter = this.categories.filter(category => category.gendre.charAt(0) == this.$route.path.substring(1).charAt(0))
-        this.menus = []
-        this.menus = this.categoriesfilter[0].children
-        this.categoriesfilter.forEach(element => {
-            this.menus.push(element)
-        });
+        this.menus = this.categoriesfilter[0].children;
       }
     },
     methods : {
       goto(id) {
         window.location.href = `http://localhost:8080/${this.categoriesfilter[0].gendre}/products/${id}`
-      },
-      ok() {
-        console.log(this.categories);
       }
-    },
-
+    }
   }
 </script>
 <style scoped>
@@ -114,15 +62,12 @@ const myQuery = gql`
     margin-top: 64px;
     box-shadow: 0px 5px 5px 0px rgba(173,173,173,1);
   }
-
   #NavbarCategory>div {
     display: inline-block;
   }
-
   .theme--light.v-btn:not(.v-btn--flat):not(.v-btn--text):not(.v-btn--outlined) {
     background-color: #fff;
   }
-
   #app .v-menu__content {
     min-width: auto !important;
     min-width: 66% !important;
